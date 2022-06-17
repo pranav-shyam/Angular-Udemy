18991a6f6a9385b9783c35446faa231186b2072c
Pranav Shyam <pranav.shyam@nslhub.com>
2022-06-17 14:12:45 +0530

    Alert Application

---
+++ src/app/alert-comp/alert-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-comp',
+  templateUrl: './alert-comp.component.html',
+  styleUrls: ['./alert-comp.component.css']
+})
+export class AlertCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { AlertCompComponent } from './alert-comp/alert-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AlertCompComponent
   ],
   imports: [
     BrowserModule
